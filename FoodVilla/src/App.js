14479644302031290e7738b9997ec5d6b7b9d8e8
Subhadeep Paul <subhadeeppaul35@gmail.com>
2023-08-28 12:49:47 +0530

    FIle added

--- FoodVilla/src/App.js
+++ FoodVilla/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Header from './components/Header';
 
 /**
          Header
@@ -19,34 +20,6 @@ import ReactDOM from 'react-dom/client';
             - Copyright
         
         */
-
-const Title = () => (
-  <a href='/'>
-    <img
-      className='logo'
-      src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQP8H2paO0fRUrqkMP4Pb5bj6sWqAITUOveg&usqp=CAU'
-      alt='logo'
-    />
-  </a>
-);
-
-//Composing Components
-
-const Header = () => {
-  return (
-    <div className='header'>
-      <Title />
-      <div className='nav-items'>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Cart</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
 
 //Config Driven UI - Mock Data from API
 const restaurantList = [
---
+++ FoodVilla/src/components/Header.js
@@ -0,0 +1,29 @@
+const Title = () => (
+  <a href='/'>
+    <img
+      className='logo'
+      src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQP8H2paO0fRUrqkMP4Pb5bj6sWqAITUOveg&usqp=CAU'
+      alt='logo'
+    />
+  </a>
+);
+
+//Composing Components
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <Title />
+      <div className='nav-items'>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
