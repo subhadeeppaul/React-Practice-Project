16c29a5563eaebf43c923ee37281495edd9181c4
Subhadeep Paul <subhadeeppaul35@gmail.com>
2023-08-29 07:42:05 +0530

    New File Added

--- FoodVilla/src/components/Body.js
+++ FoodVilla/src/components/Body.js
@@ -1,13 +1,52 @@
+import { useState } from 'react';
 import { restaurantList } from '../config';
 import RestaurantCard from './RestaurantCard';
 
+function filterData(searchText, restaurants) {
+  const filterData = restaurants.filter((restaurant) =>
+    restaurant.data.name.includes(searchText)
+  );
+  return filterData;
+}
+
 const Body = () => {
+  const [restaurants, setRestaurants] = useState(restaurantList);
+  //searchText is a local state variable
+  const [searchText, setSearchText] = useState(''); //To create state variables
+
   return (
-    <div className='restaurant-list'>
-      {restaurantList.map((restaurant) => {
-        return <RestaurantCard {...restaurant.data} key={restaurant.data.id} />;
-      })}
-    </div>
+    <>
+      <div className='search-container'>
+        <input
+          type='text'
+          className='search-input'
+          placeholder='Search'
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+
+        <button
+          className='search-btn'
+          onClick={() => {
+            //need to filter the data
+            const data = filterData(searchText, restaurants);
+            //update the state - restaurants variable
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className='restaurant-list'>
+        {restaurants.map((restaurant) => {
+          return (
+            <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+          );
+        })}
+      </div>
+    </>
   );
 };
 
